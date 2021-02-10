c06ace55edd5df955593482410bbdc57b4d2db32
Maryam <m.moh@gmx.ch>
2021-02-10 16:07:28 +0100

    DATA validation LOOPs

--- src/com/MORTGAGE/Main.java
+++ src/com/MORTGAGE/Main.java
@@ -9,22 +9,44 @@ public class Main {
         System.out.println("Mortgage Calculator: ");
         System.out.println("---------------------");
 
-
         final byte MONTH_IN_YEAR = 12;
         final byte PERCENT = 100;
+
+        int principle;
+        float monthlyInterestRate;
+        int numberOfPayments;
+
         Scanner scanner = new Scanner(System.in);
 
+        // infinite LOOP while (true){}
+        while (true) {
+            System.out.print("Principle ( € 1K - € 1M) : ");
+            principle = scanner.nextInt();
+            if (principle>= 1_000 && principle<= 1_000_000) // for one statement we need no {} for IF
+                break;
+            System.out.println("Enter a value between 1000 and 1000000 ");
+        }
 
-        System.out.print("Principle: ");
-        int principle = scanner.nextInt();
+        while (true){
+            System.out.print("AnnualInterestRate: ");
+            float annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate>=1 && annualInterestRate<=30) { // for more that 1 statement we need no {} for IF
+                monthlyInterestRate = annualInterestRate / PERCENT / MONTH_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("AnnualInterestRate: ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTH_IN_YEAR;
 
-        System.out.print("Period (year): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTH_IN_YEAR;
+        while (true) {
+            System.out.print("Period (year): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30){
+                numberOfPayments = years * MONTH_IN_YEAR;
+            break;
+        }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
 
 
